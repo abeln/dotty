scala/scala3

Don't use null literal in parser error terms

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -317,7 +317,10 @@ object Parsers {
             accept(SEMI)
         }
 
-    def errorTermTree: Literal = atPos(in.offset) { Literal(Constant(null)) }
+    /** An error tree that stands for a pattern or an expression */
+    def errorTermTree(inPattern: Boolean): Tree = atPos(in.offset) {
+      Ident(if (inPattern) nme.WILDCARD else nme.???)
+    }
 
     private[this] var inFunReturnType = false
     private def fromWithinReturnType[T](body: => T): T = {
@@ -1457,7 +1460,7 @@ object Parsers {
           if (isLiteral) literal()
           else {
             syntaxErrorOrIncomplete(IllegalStartSimpleExpr(tokenString(in.token)))
-            errorTermTree
+            errorTermTree(inPattern = false)
           }
       }
       simpleExprRest(t, canApply)
@@ -1749,7 +1752,7 @@ object Parsers {
         if (isLiteral) literal(inPattern = true)
         else {
           syntaxErrorOrIncomplete(IllegalStartOfSimplePattern())
-          errorTermTree
+          errorTermTree(inPattern = true)
         }
     }
 

abeln/dotty

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -324,7 +324,10 @@ object Parsers {
             accept(SEMI)
         }
 
-    def errorTermTree: Literal = atSpan(in.offset) { Literal(Constant(null)) }
+    /** An error tree that stands for a pattern or an expression */
+    def errorTermTree(inPattern: Boolean): Tree = atSpan(in.offset) {
+      Ident(if (inPattern) nme.WILDCARD else nme.???)
+    }
 
     private[this] var inFunReturnType = false
     private def fromWithinReturnType[T](body: => T): T = {
@@ -1501,7 +1504,7 @@ object Parsers {
           if (isLiteral) literal()
           else {
             syntaxErrorOrIncomplete(IllegalStartSimpleExpr(tokenString(in.token)), expectedOffset)
-            errorTermTree
+            errorTermTree(inPattern = false)
           }
       }
       simpleExprRest(t, canApply)
@@ -1820,7 +1823,7 @@ object Parsers {
         if (isLiteral) literal(inPattern = true)
         else {
           syntaxErrorOrIncomplete(IllegalStartOfSimplePattern(), expectedOffset)
-          errorTermTree
+          errorTermTree(inPattern = true)
         }
     }
 