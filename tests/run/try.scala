scala/scala3

Make Null a subtype of Any, instead of AnyRef
Null <: Any makes for a cleaner type hierarchy.
For example, we can now abstract over non-nullable types with
`def foo[T <: AnyRef](x: T) = ...`

However, both AnyRef and Null need to be comparable with reference
equality, so we add a new trait RefEq
```
trait RefEq {
  def eq(that: RefEq): Boolean
  def ne(that: RefEq): Boolean
}
```

and make both AnyRef and Null extend RefEq.

RefEq is completely synthetic, and it gets erased to Object.

tests/run/try.scala
@@ -38,7 +38,7 @@ object Test extends AnyRef with App {
     Console.println(x + n);
   }
 
-  var instance: AnyRef = null;
+  var instance: AnyRef|Null = null;
 
   def try4 = {
     if (instance == null) {

abeln/dotty

tests/run/try.scala
@@ -38,7 +38,7 @@ object Test extends AnyRef with App {
     Console.println(x + n);
   }
 
-  var inst: AnyRef = null;
+  var inst: AnyRef|Null = null;
 
   def try4 = {
     if (inst == null) {