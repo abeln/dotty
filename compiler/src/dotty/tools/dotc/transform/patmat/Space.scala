9b9b5bed9a520c724107f01978bf5a55b145b0ff
Abel Nieto <abel.nieto90@gmail.com>
2019-02-08 14:18:38 -0500

    Fix error introduced while adding explicit nulls flag
    If the flag isn't set, then we can't consider `null` a subtype
    of something else for the purposes of reachability checks.

--- compiler/src/dotty/tools/dotc/transform/patmat/Space.scala
+++ compiler/src/dotty/tools/dotc/transform/patmat/Space.scala
@@ -397,7 +397,11 @@ class SpaceEngine(implicit ctx: Context) extends SpaceLogic {
 
   /** Is `tp1` a subtype of `tp2`?  */
   def isSubType(tp1: Type, tp2: Type): Boolean = {
-    val res = tp1 <:< tp2
+    val res = if (!ctx.settings.YexplicitNulls.value) {
+      (tp1 != constantNullType || tp2 == constantNullType) && tp1 <:< tp2
+    } else {
+      tp1 <:< tp2
+    }
     debug.println(s"${tp1} <:< ${tp2} = $res")
     res
   }
