scala/scala3

Strip away JavaNull during implicit conversion search
When looking for an implicit conversion from A|JavaNull -> B, if we
can't find it, then also look for an implicit conversion from A -> B.

However, only strip `JavaNull` away, but not `Null`.

compiler/src/dotty/tools/dotc/typer/Implicits.scala
@@ -604,8 +604,15 @@ trait Implicits { self: Typer =>
           SelectionProto(name, memberProto, compat, privateOK = false)
         case tp => tp
       }
-      try inferImplicit(adjust(to), from, from.pos)
-      catch {
+      try {
+        val to1 = adjust(to)
+        inferImplicit(to1, from, from.pos) match {
+          case err: SearchFailure if !err.isAmbiguous && from.tpe.isJavaNullableUnion =>
+            val from1 =  from.ensureConforms(from.tpe.stripJavaNull)
+            inferImplicit(to1, from1, from.pos)
+          case res => res
+        }
+      } catch {
         case ex: AssertionError =>
           implicits.println(s"view $from ==> $to")
           implicits.println(ctx.typerState.constraint.show)

abeln/dotty

compiler/src/dotty/tools/dotc/typer/Implicits.scala
@@ -620,8 +620,15 @@ trait Implicits { self: Typer =>
           SelectionProto(name, memberProto, compat, privateOK = false)
         case tp => tp
       }
-      try inferImplicit(adjust(to), from, from.span)
-      catch {
+      try {
+        val to1 = adjust(to)
+        inferImplicit(to1, from, from.span) match {
+          case err: SearchFailure if !err.isAmbiguous && from.tpe.isJavaNullableUnion =>
+            val from1 =  from.ensureConforms(from.tpe.stripJavaNull)
+            inferImplicit(to1, from1, from.span)
+          case res => res
+        }
+      } catch {
         case ex: AssertionError =>
           implicits.println(s"view $from ==> $to")
           implicits.println(ctx.typerState.constraint.show)